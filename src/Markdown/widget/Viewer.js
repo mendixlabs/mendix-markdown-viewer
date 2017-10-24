423f8e2229a3ae3a3d39bccb02eac013c6b961af
JelteMX <jelte.lagendijk@mendix.com>
2017-10-24 12:17:57 +0200

    Cleanup

--- src/Markdown/widget/Viewer.js
+++ src/Markdown/widget/Viewer.js
@@ -9,9 +9,24 @@ import Markdown from 'markdown-it';
 import prism from 'markdown-it-prism';
 import MarkdownItContainer from 'markdown-it-container';
 
-import {
-    AlertContainer,
-} from './containers';
+const AlertContainer = md => {
+    return {
+        validate: params => {
+            return params.trim().match(/^alert\s+(.*)$/);
+        },
+        render: (tokens, idx) => {
+            const m = tokens[ idx ].info.trim().match(/^alert\s+(.*)$/);
+
+            if (1 === tokens[ idx ].nesting) {
+                // opening tag
+                return `<div class="alert alert-${md.utils.escapeHtml(m[ 1 ])}">\n`;
+            }
+
+            // closing tag
+            return '</div>\n';
+        },
+    };
+};
 
 import './Viewer.scss';
 import 'prismjs/themes/prism.css';
