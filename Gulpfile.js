d0d92e84f477aaa438ae6db2dc8cc0f07d9d5914
Maismaus <mail@mariusvanderknaap.nl>
2022-09-01 13:34:50 +0200

    Upgrade to Gulp v4

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,20 +1,19 @@
-const path = require('path');
-const fs = require('fs-extra');
-const gulp = require('gulp-help')(require('gulp'));
-const gutil = require('gulp-util');
-const webpack = require('webpack');
-const watch = require('gulp-watch');
-const gulpCopy = require('gulp-copy');
-const sequence = require('gulp-sequence');
-const del = require('del');
+const path = require("path");
+const fs = require("fs-extra");
+const gulp = require("gulp");
+const gutil = require("gulp-util");
+const webpack = require("webpack");
+const watch = require("gulp-watch");
+const sequence = require("gulp-sequence");
+const del = require("del");
 
-const banner = (color, banner) => gutil.colors[color || 'cyan'](banner ? `[${banner}]` : '[GULP]');
+const banner = (color, banner) => gutil.colors[color || "cyan"](banner ? `[${banner}]` : "[GULP]");
 
-const pkg = require('./package.json');
+const pkg = require("./package.json");
 
 // Set paths for our project folder
 
-const projectPath = pkg.widget.path ? path.resolve(pkg.widget.path) + '/' : false;
+const projectPath = pkg.widget.path ? path.resolve(pkg.widget.path) + "/" : false;
 const widgetsFolder = projectPath ? path.join(projectPath, `/widgets/`) : false;
 const deploymentFolder = projectPath ? path.join(projectPath, `/deployment/web/widgets/`) : false;
 
@@ -22,36 +21,39 @@ const deploymentFolder = projectPath ? path.join(projectPath, `/deployment/web/w
 
 let stat = null;
 if (!projectPath) {
-    gutil.log(`${banner()} No testproject defined, only copying files to dist/build folder. Set project path in ${gutil.colors.cyan('widget.path')} in ${gutil.colors.magenta('package.json')}`);
+    gutil.log(`${banner()} No testproject defined, only copying files to dist/build folder. Set project path in ${gutil.colors.cyan("widget.path")} in ${gutil.colors.magenta("package.json")}`);
 } else {
     gutil.log(`${banner()} Testproject defined: ${gutil.colors.magenta(projectPath)}`);
     try {
         stat = projectPath ? fs.statSync(projectPath) : null;
     } catch (e) {
-        gutil.log(`${banner('red')} Error getting project directory:`, e.toString());
-        gutil.log(`${banner('red')} Copying to the project directory has been disabled`);
+        gutil.log(`${banner("red")} Error getting project directory:`, e.toString());
+        gutil.log(`${banner("red")} Copying to the project directory has been disabled`);
         stat = null;
     }
 }
 
 // Helper functions
 
-const runWebpack = callback => {
-    webpack(require('./webpack.config.js'), function (err, stats) {
+const runWebpack = (callback) => {
+    webpack(require("./webpack.config.js"), function (err, stats) {
         if (err) throw new gutil.PluginError("webpack", err);
-        gutil.log(banner('cyan', 'WEBPACK'), stats.toString({
-            colors: true,
-            modules: false
-        }));
+        gutil.log(
+            banner("cyan", "WEBPACK"),
+            stats.toString({
+                colors: true,
+                modules: false,
+            })
+        );
         callback && callback();
     });
 };
 
-const copyFile = paths => {
+const copyFile = (paths) => {
     try {
         fs.copySync(paths.src, paths.dest);
     } catch (err) {
-        gutil.log(`${banner('red')} Copy fail`, err);
+        gutil.log(`${banner("red")} Copy fail`, err);
     }
 };
 
@@ -59,59 +61,74 @@ const getPaths = (file, srcFolder, destFolder) => {
     return {
         src: path.join(__dirname, srcFolder, file.relative),
         dest: path.join(destFolder, file.relative),
-    }
-}
+    };
+};
 
 // Base tasks
 
-gulp.task('watch:src', () => {
-    return watch('src/**/*', {
-        verbose: true
-    }, () => {
-        runWebpack();
-    })
+gulp.task("watch:src", () => {
+    return watch(
+        "src/**/*",
+        {
+            verbose: true,
+        },
+        () => {
+            runWebpack();
+        }
+    );
 });
 
-gulp.task('watch:build', () => {
-    return watch('build/**/*', {
-        verbose: stat !== null,
-        read: false
-    }, file => {
-        if (stat !== null) {
-            const paths = getPaths(file, 'build', deploymentFolder);
-            if (paths.src.indexOf('package.xml') !== -1) {
-                return;
+gulp.task("watch:build", () => {
+    return watch(
+        "build/**/*",
+        {
+            verbose: stat !== null,
+            read: false,
+        },
+        (file) => {
+            if (stat !== null) {
+                const paths = getPaths(file, "build", deploymentFolder);
+                if (paths.src.indexOf("package.xml") !== -1) {
+                    return;
+                }
+                copyFile(paths);
             }
-            copyFile(paths);
         }
-    })
+    );
 });
 
-gulp.task('watch:dist', () => {
-    return watch(`dist/${pkg.widget.package}.mpk`, {
-        verbose: stat !== null,
-        read: false
-    }, file => {
-        if (stat !== null) {
-            const paths = getPaths(file, 'dist', widgetsFolder);
-            copyFile(paths);
+gulp.task("watch:dist", () => {
+    return watch(
+        `dist/${pkg.widget.package}.mpk`,
+        {
+            verbose: stat !== null,
+            read: false,
+        },
+        (file) => {
+            if (stat !== null) {
+                const paths = getPaths(file, "dist", widgetsFolder);
+                copyFile(paths);
+            }
         }
-    })
+    );
 });
 
-gulp.task('clean', `Cleanup the dist/build`, () => {
-    return del([
-        'dist',
-        'build'
-    ], { force: true });
-});
+const clean = () => del(["dist", "build"], { force: true });
+const buildDist = (callback) => runWebpack(callback);
+// gulp.task("clean", `Cleanup the dist/build`, () => {
+//     return ;
+// });
 
 // Final tasks
 
-gulp.task('build', 'Build the widget', done => {
-    sequence('clean', 'build-dist', done);
-});
+// gulp.task("build", "Build the widget", (done) => {
+//     sequence("clean", "build-dist", done);
+// });
 
-gulp.task('build-dist', callback => { runWebpack(callback); });
+// gulp.task("build-dist", (callback) => {
+//     runWebpack(callback);
+// });
 
-gulp.task('default', ['watch:src', 'watch:build', 'watch:dist']);
+// gulp.task("default", gulp.series("watch:src", "watch:build", "watch:dist"));
+const build = gulp.series(clean, buildDist);
+exports.build = build;
